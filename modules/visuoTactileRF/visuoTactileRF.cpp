4073d6a7d8ada3fb431bbe769a2e56c8cc4c54f6
Alessandro Roncone <alecive87@gmail.com>
2014-11-03 17:52:16 +0100

    [vtRF] Added the possibility to select between individual body parts

--- modules/visuoTactileRF/visuoTactileRF.cpp
+++ modules/visuoTactileRF/visuoTactileRF.cpp
@@ -252,11 +252,52 @@ public:
                 {
                     Bottle *taxelPosFiles = skinEventsConf.find("taxelPositionFiles").asList();
 
-                    for(int i=0;i<partNum;i++)     // all of the skinparts
+                    if (rf.check("--leftHand") || rf.check("--leftForeArm") || rf.check("--rightHand") || rf.check("--rightForeArm"))
                     {
-                        // if (i==2)                   // only right hand
-                        if (i==3)                      // only right forearm
-                        // if (i==1)                         // only left forearm
+                        if (rf.check("--leftHand"))
+                        {
+                            string taxelPosFile = taxelPosFiles->get(0).asString().c_str();
+                            string filePath(skinRF.findFile(taxelPosFile.c_str()));
+                            if (filePath!="")
+                            {
+                                printf("*** VisuoTactileRF: filePath [%i] %s\n",0,filePath.c_str());
+                                filenames.push_back(filePath);
+                            }
+                        }
+                        if (rf.check("--leftForeArm"))
+                        {
+                            string taxelPosFile = taxelPosFiles->get(1).asString().c_str();
+                            string filePath(skinRF.findFile(taxelPosFile.c_str()));
+                            if (filePath!="")
+                            {
+                                printf("*** VisuoTactileRF: filePath [%i] %s\n",1,filePath.c_str());
+                                filenames.push_back(filePath);
+                            }
+                        }
+                        if (rf.check("--rightHand"))
+                        {
+                            string taxelPosFile = taxelPosFiles->get(2).asString().c_str();
+                            string filePath(skinRF.findFile(taxelPosFile.c_str()));
+                            if (filePath!="")
+                            {
+                                printf("*** VisuoTactileRF: filePath [%i] %s\n",2,filePath.c_str());
+                                filenames.push_back(filePath);
+                            }
+                        }
+                        if (rf.check("--rightForeArm"))
+                        {
+                            string taxelPosFile = taxelPosFiles->get(3).asString().c_str();
+                            string filePath(skinRF.findFile(taxelPosFile.c_str()));
+                            if (filePath!="")
+                            {
+                                printf("*** VisuoTactileRF: filePath [%i] %s\n",3,filePath.c_str());
+                                filenames.push_back(filePath);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        for(int i=0;i<partNum;i++)     // all of the skinparts
                         {
                             string taxelPosFile = taxelPosFiles->get(i).asString().c_str();
                             string filePath(skinRF.findFile(taxelPosFile.c_str()));
